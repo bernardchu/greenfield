762e47ec4c9ee8765354964fc2f2d148d4e59e31
Bernard Chu <bernard.chu@yahoo.com>
2014-12-09 17:51:21 -0800

    Add placeholder functions for GET and POST requests at /charity in charity controller.

--- server/charity/charityRoutes.js
+++ server/charity/charityRoutes.js
@@ -2,7 +2,7 @@ var charityController = require('./charityController');
 
 module.exports = function(app) {
   app.get('/:orgid', charityController.fetch);
-  app.get('/charity/category'), charityController.sendCategories;
-  app.get('/charity/subCategory'), charityController.sendSubCategories;
-  app.post('/charity'), charityController.register);
+  app.get('/charity/category', charityController.sendCategories);
+  app.get('/charity/subCategory', charityController.sendSubCategories);
+  app.post('/charity', charityController.register);
 };
