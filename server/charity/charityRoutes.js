f7b2c3540d9cd4b72c9b69c3d877e5677611ea2f
Bernard Chu <bernard.chu@yahoo.com>
2014-12-09 17:40:56 -0800

    Add routes for GET and POST requests to /charity.

--- server/charity/charityRoutes.js
+++ server/charity/charityRoutes.js
@@ -2,4 +2,7 @@ var charityController = require('./charityController');
 
 module.exports = function(app) {
   app.get('/:orgid', charityController.fetch);
+  app.get('/charity/category'), charityController.sendCategories('main');
+  app.get('/charity/subCategory'), charityController.sendCategories('sub');
+  app.post('/charity'), charityController.register);
 };
