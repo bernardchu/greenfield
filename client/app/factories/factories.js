0ecd18ca1226f044baf62d909d2c332f393af0c3
Bernard Chu <bernard.chu@yahoo.com>
2014-12-10 10:48:38 -0800

    Add factory to get category and subCategory info from server.

--- client/app/factories/factories.js
+++ client/app/factories/factories.js
@@ -60,4 +60,31 @@ angular.module('pledgr.factories', [])
     sendCode: sendCode,
     verifyCode: verifyCode
   };
+})
+
+.factory('Categories', function($http) {
+  var getCategories = function() {
+    return $http({
+      method: 'GET',
+      url: '/api/charity/category'
+    })
+    .then(function(resp) {
+      return resp.data;
+    });
+  };
+
+  var getSubCategories = function() {
+    return $http({
+      method: 'GET',
+      url: '/api/charity/subCategory'
+    })
+    .then(function(resp) {
+      return resp.data;
+    });
+  };
+
+  return {
+    getCategories: getCategories,
+    getSubCategories: getSubCategories
+  }
 });
