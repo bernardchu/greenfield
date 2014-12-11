4954933e8929a7a08fb6f3cfef39f0b699801bc8
Issac <issac@Issacs-MacBook-Air.local>
2014-12-10 17:36:18 -0800

    implemented basic stripe functionality

--- server/card/cardController.js
+++ server/card/cardController.js
@@ -12,30 +12,32 @@ module.exports = {
   	    //if customer ID does not exist, then create a new customer
   	    stripe.customers.create(
   	    	{
-  	      	// card: stripeToken,
-  	      	description: '12345@hackreactor.com'
+  	      	description: 'someone@example.com'
   	    	}, 
   	    	function(err, customer) {
   	    		if(err) {
   	    			res.send('failed to create customer!');
   	    		}
+  	    		console.log("CUSTOMER = ", customer);
 	  				stripe.customers.createCard(customer.id, {card: stripeToken},
 	  				  function(err, card) {
 	  				  	if(err) {
 	  				  		console.log('ERROR', err);
 	  				  	}
 	  				    if(!err) {   	
-	  				    	// console.log('CARD IS', card);
+	  				    	console.log('CARD IS', card);
 				  	      return stripe.charges.create({
-				  	        // amount: 50,
+				  	        amount: 1000,
 				  	        currency: "usd",
 				  	        customer: customer.id
 				  	      }, 
 				  	      function(err, charge) {
-  	        	    	// console.log('CHARGE IS', charge);
+  	        	    	console.log('CHARGE IS', charge);
 
   	        	    	var card = new Card({user: req.body.user, customer_id: customer.id});
-  	            		card.save(function(error, card) {
+  	        	    	console.log('CARD IS', card);
+  	            		card.save(function(error) {
+  	            			console.log("made it in here!!!");
   	              		if (!error) {
   	              			res.send('SUCCESS');
   	              		}
