5bb76595aa87be114563d153d2d92b2ba6cbb2ea
Issac <issac@Issacs-MacBook-Air.local>
2014-12-11 14:28:29 -0800

    finished basic stripe functionality

--- server/card/cardController.js
+++ server/card/cardController.js
@@ -12,39 +12,31 @@ module.exports = {
   	    //if customer ID does not exist, then create a new customer
   	    stripe.customers.create(
   	    	{
-  	      	description: 'someone@example.com'
+  	      	// card: stripeToken,
+  	      	description: '12345@hackreactor.com'
   	    	}, 
   	    	function(err, customer) {
   	    		if(err) {
   	    			res.send('failed to create customer!');
   	    		}
-  	    		console.log("CUSTOMER = ", customer);
 	  				stripe.customers.createCard(customer.id, {card: stripeToken},
-	  				  function(err, card) {
-	  				  	if(err) {
-	  				  		console.log('ERROR', err);
-	  				  	}
-	  				    if(!err) {   	
-	  				    	console.log('CARD IS', card);
-				  	      return stripe.charges.create({
-				  	        amount: 1000,
-				  	        currency: "usd",
-				  	        customer: customer.id
-				  	      }, 
-				  	      function(err, charge) {
-  	        	    	console.log('CHARGE IS', charge);
-
-  	        	    	var card = new Card({user: req.body.user, customer_id: customer.id});
-  	        	    	console.log('CARD IS', card);
-  	            		card.save(function(error) {
-  	            			console.log("made it in here!!!");
-  	              		if (!error) {
-  	              			res.send('SUCCESS');
-  	              		}
-  	            		});
-				  	      });
-	  				    }
-	  				});
+  				  function(err, card) {
+  				    if(!err) {   	
+			  	      return stripe.charges.create({
+			  	        // amount: 1000,
+			  	        currency: "usd",
+			  	        customer: customer.id
+			  	      }, 
+			  	      function(err, charge) {
+	        	    	var card = new Card({user: req.body.user, customer_id: customer.id});
+	            		card.save(function(error) {
+	              		if (!error) {
+	              			res.send('SUCCESS');
+	              		}
+	            		});
+			  	      });
+  				    }
+  				});
   	    });
       }
       // else {
