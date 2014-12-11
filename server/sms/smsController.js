e225ecf152faef750857624e6895c981129a1931
Bernard Chu <bernard.chu@yahoo.com>
2014-12-11 13:51:17 -0800

    Begin modifying SMS controller to charge card upon user response to SMS.

--- server/sms/smsController.js
+++ server/sms/smsController.js
@@ -3,7 +3,12 @@ var AuthCodeModel = require('./authCodeModel');
 var SmsModel = require('./sentMessagesModel');
 var UserModel = require('../users/userModel');
 var CharityModel = require('../charity/charityModel');
+<<<<<<< HEAD
 var CardModel = require('../card/cardModel');
+=======
+var stripe = require('stripe')("sk_test_WjBhk0wLLvaJp3bO65bozL53");
+var CardModel = require('../card/cardModel.js')
+>>>>>>> Begin modifying SMS controller to charge card upon user response to SMS.
 
 var client = require('twilio')(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
 
@@ -41,15 +46,23 @@ module.exports = {
             var today = new Date();
             // Set the donation to the amount of the yearly pledge divided by the number of weeks remaining in the year
             var donationAmount = Math.round((data.pledge / (53 - weekNumber(today))) * 100) / 100;
-            // CardModel.findOne({user: data.username}, function(err, card) {
-            //   if(!err) {
-            //     stripe.charges.create({
-            //       amount: donationAmount, // amount in cents, again
-            //       currency: "usd",
-            //       customer: card.customer_id
-            //     });
-            //   }
-            // })
+
+            // Find the user's card
+            CardModel.findOne({ user: data.username }, function(err, cardData) {
+              // if error (card doesn't exist) then we have no card on file.
+              if (err) {
+                console.log('No card on file: ',err);
+                res.send('User has no card on file.');
+              }
+              // else charge the card
+              stripe.charges.create({
+                amount: donationAmount, // amount in cents, again
+                currency: "usd",
+                customer: cardData.customer_id
+              }, function(err, charge) {
+                console.log('New charge: ', charge);
+              });
+            });
             // Create a new donation in the donations collection
             var donation = new SmsModel.Donations({
               phone: data.phone,
